b35d6fad9d6b79a2b6ced8ae84865c7060414a99
plvs vltra <justemailpostbox@gmail.com>
2022-07-04 15:29:24 +0500

    New style and base funcs

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,98 @@
-function createTask(text, state = false, proirity = 1){
+let todo = document.getElementById("todo"),
+    list = document.getElementById("todo").children[1],
+    input = document.getElementById("input"),
+    input_defautText =  "Type new task here...";
+
+
+
+function createTask(text, state = false, editable = false, proirity = 1){
     return {
         text: text,
         state: state,
         proirity: proirity,
+        editable: editable,
+        html: null,
     }
 }
 
-let task = createTask("Some task");
 
-console.log(task);
+
+function addTaskTo(where, task){
+    let taskHTML_li = document.createElement("li"),
+        taskHTML_checkbox = document.createElement("input"),
+        taskHTML_text = document.createElement("div");
+
+        taskHTML_text.classList = "task-text";
+
+        taskHTML_checkbox.setAttribute("type", "checkbox");
+        taskHTML_text.innerHTML = task.text;
+
+        taskHTML_li.appendChild(taskHTML_checkbox);
+        taskHTML_li.appendChild(taskHTML_text);
+
+        task.html = taskHTML_li;
+        console.log(where.lastChild)
+        where.insertBefore(taskHTML_li, where.children[where.children.length - 1]);
+
+        taskHTML_checkbox.addEventListener("click", ()=> {
+            task.state = task.state ? false : true;
+
+            if(!task.state) taskHTML_text.classList.add("pale");
+            if(task.state) taskHTML_text.classList.remove("pale");
+
+            taskHTML_checkbox.classList = task.state == false ? "pale" : "";
+            taskHTML_text.setAttribute("contenteditable", task.state);
+        });
+
+        taskHTML_text.addEventListener('mouseover', ()=>{
+           if(task.state === true){
+            task.editable = task.editable ? false : true;
+            taskHTML_text.setAttribute("contenteditable", task.editable);
+           }
+        });
+
+        taskHTML_text.addEventListener('mouseout', ()=>{
+            task.editable = task.editable ? false : true;
+            task.text = taskHTML_text.innerHTML;
+        });
+
+}
+
+
+
+
+input.addEventListener("click", () => {
+    if(input.innerHTML == input_defautText){
+        input.innerHTML = "";
+        input.classList = "";
+    }
+});
+
+
+
+document.body.addEventListener("click", event =>{
+    let target = event.path[0];
+
+    // console.log(target.classList);
+
+    if(target.classList[0] !== "task-text" && target.id !== "input"){
+        let task = createTask(input.innerHTML, true);
+        if(input.innerHTML !== "" && input.innerHTML !== input_defautText) addTaskTo(list, task);
+        input.innerHTML = input_defautText;
+        input.classList = "pale";
+        console.log(input)
+    }
+});
+
+
+
+
+input.addEventListener("keydown", event => {
+    if(event.key == "Enter"){
+        let task = createTask(input.innerHTML, true);
+        if(input.innerHTML !== "" && input.innerHTML !== input_defautText) addTaskTo(list, task);
+        input.innerHTML = "";
+        input.textContent = "";
+        input.classList = "pale";
+    }
+});
